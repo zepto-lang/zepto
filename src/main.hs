40305a3a8f041c980d604cdd651f4afdb477a146
Veit Heller <veit.heller@student.htw-berlin.de>
2015-01-11 23:30:07 +0100

    Fixed typo in usage and refactored

--- src/main.hs
+++ src/main.hs
@@ -2,14 +2,14 @@ module Main where
 import Prompt
 import System.Environment
 
+printUsage :: IO ()
+printUsage = putStrLn("R5RS version 0.1\nUsage: " ++
+                    "\n\twithout arguments - runs REPL" ++
+                    "\n\t1 argument - executes single statement")
 
 main :: IO ()
 main = do args <- getArgs
           case length args of
                 0 -> runRepl
                 1 -> evalAndPrint $ args !! 0
-                otherwise -> 
-                    putStrLn("R5RS version 0.1" ++
-                    "\nUsage: " ++
-                    "\n\twithout arguments - runs REPL" ++
-                    "\n\t1 arguments - executes single statement")
+                otherwise -> printUsage
