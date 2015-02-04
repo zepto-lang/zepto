2d775ebe3821b35c7a3b259db6c82c9b51ac514b
Veit Heller <veit.heller@student.htw-berlin.de>
2015-02-04 10:15:43 +0100

    Added a few utility prompts

--- src/main.hs
+++ src/main.hs
@@ -3,14 +3,24 @@ import Prompt
 import System.Environment
 
 printUsage :: IO ()
-printUsage = putStrLn("R5RS version 0.1\nUsage: " ++
+printUsage = do printVersion
+                putStrLn("Usage: " ++
                     "\n\twithout arguments\t- runs REPL" ++
                     "\n\t1 argument\t\t- executes single statement")
+
+printVersion :: IO ()
+printVersion = putStrLn "R5RS Version 0.2"
+
+printCommands :: IO ()
+printCommands = putStrLn "Type 'quit' or press Ctrl-C to exit interpreter"
 
 main :: IO ()
 main = do args <- getArgs
           if null args
-              then runRepl
+              then do printVersion
+                      printCommands
+                      putStrLn ""
+                      runRepl
               else 
                   if ((args !! 0) ==  "-h") then printUsage 
                   else runSingleStatement $ args
