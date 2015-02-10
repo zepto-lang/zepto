f7aab1ded16c965191a3bd363c5315a878c65801
Veit Heller <veit.heller@student.htw-berlin.de>
2015-02-10 19:28:17 +0100

    Added comments to math 'library'

--- stdlib/math.scm
+++ stdlib/math.scm
@@ -1,3 +1,5 @@
+; This is purely homegrown math. It's best you don't use it.
+; Not even once.
 (define (precision x) (* x 1.0))
 (define (abs x) ((if (< x 0) - +)  x))
 (define (almost-equal x y delta) 
@@ -7,6 +9,7 @@
     (if (almost-equal (/ (+ x last-x) 2) x 0.000001) (/ x 2)
         (sqrt-prime (/ (+ x last-x) 2) x)))
 
+; This routine is broken
 (define (sqrt x) (sqrt-prime (precision x) 1))
 
 (define (quad x) (* x x))
