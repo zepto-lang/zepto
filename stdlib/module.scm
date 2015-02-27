cfcc461d8edcad68af407cca79b2e12dbe22e41d
Veit Heller <veit.heller@student.htw-berlin.de>
2015-02-27 20:32:38 +0100

    Added random module

--- stdlib/module.scm
+++ stdlib/module.scm
@@ -4,7 +4,7 @@
 (load "delay.scm")
 (load "logical.scm")
 (load "pairs.scm")
-;; (load "random.scm")
+(load "random.scm")
 (load "util.scm")
 
 (load "definitions.scm")
---
+++ stdlib/random.scm
@@ -0,0 +1,16 @@
+;; This code is taken from:
+;; http://stackoverflow.com/questions/14674165/scheme-generate-random
+;; It is not to be used in cryptography or related fields.
+(define random
+  (let ((a 69069) (c 1) (m (expt 2 32)) (seed 19380110.0))
+    (lambda new-seed
+      (if (pair? new-seed)
+        (begin (set! seed (car new-seed)))
+        (begin (set! seed (modulo (+ (* seed a) c) m))))
+      (/ seed m))))
+
+(define (randint . args) "generate a random integer between the given args(the lower range is optional)"
+  (cond ((= (length args) 1) (randint 0 (car args)))
+        ((= (length args) 2)
+         (+ (car args) (floor (* (random) (- (cadr args) (car args))))))
+        (else (write "usage: (randint [lo] hi)"))))
