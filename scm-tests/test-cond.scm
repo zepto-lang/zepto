36e5c1dd67bdba2dbd9f506f9a302db0b8755c2f
Veit Heller <veit.heller@student.htw-berlin.de>
2015-03-01 10:16:31 +0100

    Moved test cases

---
+++ scm-tests/test-cond.scm
@@ -0,0 +1,9 @@
+(load "scm-tests/skim-unit.scm")
+
+(assert-equal (lambda () (cond ((> 3 2) 'greater) ((< 3 2) 'less)))
+			  'greater)
+(assert-equal (lambda () (cond ((> 3 3) 'greater) ((< 3 3) 'less) (else 'equal)))
+			  'equal)
+
+(unit-test-handler-results)
+(unit-test-all-passed)
---
+++ scm-tests/test-stdlib.scm
@@ -0,0 +1,28 @@
+(load "scm-tests/skim-unit.scm")
+
+(assert-equal (lambda () (map (curry + 2) '(1 2 3 4))) 
+			  '(3 4 5 6))
+
+(assert-equal (lambda () (filter even? '(1 2 3 4))) 
+			  '(2 4))
+
+(assert-equal (lambda () (zero? 0)) #t)
+(assert-equal (lambda () (length '(0 1 2 3))) 4)
+; (assert-equal (lambda () (member 1 '(1 2 3 4))) 1)
+
+
+(assert-equal (lambda () (memq 'a '(a b c))) '(a b c))
+(assert-equal (lambda () (memq 'b '(a b c))) '(b c))
+(assert-equal (lambda () (memq 'a '(b c d))) #f)
+; (assert-equal (lambda () (memq (list 'a) '(b (a) c))) #f)
+(assert-equal (lambda () (member (list 'a) '(b (a) c))) '((a) c))
+(assert-equal (lambda () (memv 101 '(100 101 102))) '(101 102))
+
+(assert-equal (lambda () (list-tail '(a b c d e f g) 5)) '(f g))
+(assert-equal (lambda () (list-ref '(a b c d) 2)) 'c)
+
+(assert-equal (lambda () (append '(1 2 3 4 5) '(6 7 "eight" "nine"))) 
+              '(1 2 3 4 5 6 7 "eight" "nine"))
+
+(unit-test-handler-results)
+(unit-test-all-passed)
