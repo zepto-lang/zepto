74d1a5e30599bb89277e93c68dfd79bcd921980d
hellerve <veit.heller@student.htw-berlin.de>
2015-03-08 21:44:09 +0100

    Added advanced quine

---
+++ examples/quine_advanced.scm
@@ -0,0 +1,11 @@
+((lambda (q)
+   (quasiquote
+     ((unquote q)
+      (quote
+        (unquote q)))))
+ (quote
+   (lambda (q)
+     (quasiquote
+       ((unquote q)
+        (quote
+          (unquote q)))))))
