2dc07bd0f8aebc3f8a387da4877ae27576309910
hellerve <veit.heller@student.htw-berlin.de>
2015-03-18 14:24:07 +0100

    Added tests for char functions

---
+++ scm-tests/test-char.scm
@@ -0,0 +1,16 @@
+(load "scm-tests/skim-unit.scm")
+
+(assert-equal (lambda () (char? #\a)) #t)
+(assert-equal (lambda () (char? "a")) #f)
+
+(assert-equal (lambda () (char=? #\a #\a)) #t)
+(assert-equal (lambda () (char=? #\a #\b)) #f)
+(assert-equal (lambda () (char-ci=? #\A #\a)) #t)
+(assert-equal (lambda () (char-ci=? #\A #\b)) #f)
+(assert-equal (lambda () (char>? #\b #\a)) #t)
+(assert-equal (lambda () (char>? #\a #\a)) #f)
+(assert-equal (lambda () (char-ci>? #\b #\A)) #t)
+(assert-equal (lambda () (char-ci>? #\a #\A)) #f)
+
+(unit-test-handler-results)
+(unit-test-all-passed)
