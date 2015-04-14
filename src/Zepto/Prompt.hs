dab88468364b46749f6fab5dd876ffee37608da4
hellerve <veit.heller@student.htw-berlin.de>
2015-04-14 12:54:53 +0200

    travis experiment

--- src/Zepto/Prompt.hs
+++ src/Zepto/Prompt.hs
@@ -3,7 +3,7 @@ module Zepto.Prompt( runRepl
                     , evalAndPrint
                     , runFile
                     ) where
-import Control.Applicative
+--import Control.Applicative ((<*>))
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Char
