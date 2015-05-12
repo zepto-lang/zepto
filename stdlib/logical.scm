d22f1a6e5f5b0db8b66e34fcdc77c0cc45c49341
hellerve <veit.heller@student.htw-berlin.de>
2015-05-12 18:38:55 +0200

    Added stdlib module

--- stdlib/logical.scm
+++
@@ -1,4 +0,0 @@
-(define (and . lst) "logical and on multiple values" (fold && #t lst))
-(define (or . lst) "logical or on multiple values" (fold || #f lst))
-
-(define (not x) "logical not" (if x #f #t))
