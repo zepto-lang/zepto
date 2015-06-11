4c8cb52c6a8cd8890662f50f620292fe00c32ff8
hellerve <veit.heller@student.htw-berlin.de>
2015-06-11 15:06:10 +0200

    added number->string

--- src/Zepto/Primitives/ConversionPrimitives.hs
+++ src/Zepto/Primitives/ConversionPrimitives.hs
@@ -7,13 +7,14 @@ import Zepto.Types
 symbol2String :: [LispVal] -> ThrowsError LispVal
 symbol2String ([Atom a]) = return $ String a
 symbol2String [notAtom] = throwError $ TypeMismatch "symbol" notAtom
-symbol2String [] = return $ Bool False
-symbol2String _ = return $ Bool False
+symbol2String [] = throwError $ NumArgs 1 []
+symbol2String args@(_ : _) = throwError $ NumArgs 1 args
 
 string2Symbol :: [LispVal] -> ThrowsError LispVal
 string2Symbol [] = return $ Bool False
 string2Symbol ([String s]) = return $ Atom s
 string2Symbol [notString] = throwError $ TypeMismatch "string" notString
+string2Symbol [] = throwError $ NumArgs 1 []
 string2Symbol args@(_ : _) = throwError $ NumArgs 1 args
 
 charToInteger :: [LispVal] -> ThrowsError LispVal
@@ -22,3 +23,8 @@ charToInteger ([Character c]) = return $ Number $ NumI $ toInteger $ ord c
 charToInteger [notChar] = throwError $ TypeMismatch "character" notChar
 charToInteger args@(_ : _) = throwError $ NumArgs 1 args
 
+number2String :: [LispVal] -> ThrowsError LispVal
+number2String ([Number x]) = return $ String $ show x
+number2String [notNumber] = throwError $ TypeMismatch "number" notNumber
+number2String [] = throwError $ NumArgs 1 []
+number2String args@(_ : _) = throwError $ NumArgs 1 args
