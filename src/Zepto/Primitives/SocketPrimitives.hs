c05c40a2d1b0717458c49021c7ed95a52ff56a23
hellerve <veit.heller@student.htw-berlin.de>
2016-11-05 11:47:53 +0100

    documented networking primitives

--- src/Zepto/Primitives/SocketPrimitives.hs
+++ src/Zepto/Primitives/SocketPrimitives.hs
@@ -18,7 +18,7 @@ lookupType s = case s of
                 _                -> NS.NoSocketType
 
 socketDoc :: String
-socketDoc = "create a new socket; takes a family <par>f</par>,
+socketDoc = "create a new socket; takes a family <par>f</par>,\n\
 a type <par>t</par>and a protocol number <par>p</par>.\n\
 \n\
   params:\n\
@@ -73,6 +73,24 @@ ls2AddrInfo (List [List flags,
             parsefl _ = read ""
 ls2AddrInfo _ = Nothing
 
+getAddrInfoDoc :: String
+getAddrInfoDoc = "get address info for a host <par>host</par> and port <par>port</par>.\n\
+Needed to <fun>net:connect</fun> or <dun>net:bind-socket</fun> a socket.\n\
+Thin wrapper around the getaddrinfo POSIX function.\n\
+\n\
+Example:\n\
+<zepto>\n\
+; create a socket and bind it to 127.0.0.1:5000\n\
+(net:bind-socket (net:socket \"stream\")\n\
+                 (net:get-addr-info \"127.0.0.1\" \"5000\"))\n\
+</zepto>\n\
+\n\
+  params:\n\
+    - host: the host as a string\n\
+    - port: the port as a string\n\
+  complexity: O(1)\n\
+  returns: an opaque datatype signifying an address info struct"
+
 getAddrInfo :: [LispVal] -> IOThrowsError LispVal
 getAddrInfo [addrInfo@(List _), SimpleVal n, SimpleVal s] = do
         let info = ls2AddrInfo addrInfo
@@ -92,6 +110,17 @@ getAddrInfo [_, x, SimpleVal _] = throwError $ TypeMismatch "string/nil" x
 getAddrInfo [_, _, x] = throwError $ TypeMismatch "string/nil" x
 getAddrInfo x = throwError $ NumArgs 2 x
 
+connectDoc :: String
+connectDoc = "connect a socket <par>sock</par> to an address <par>addr</par>\n\
+defined by <fun>net:get-addr-info</fun>. The socket can then be used for data\n\
+transmission.\n\
+\n\
+  params:\n\
+    - sock: the socket to connect\n\
+    - addr: a socket address as described through <fun>net:get-addr-info</fun>\n\
+  complexity: O(1)\n\
+  returns: nil"
+
 connect :: [LispVal] -> IOThrowsError LispVal
 connect [sock@(Opaque _),
          addrInfo@(Opaque _)] = do
@@ -108,6 +137,17 @@ connect [x, List _] = throwError $ TypeMismatch "list" x
 connect [_, x] = throwError $ TypeMismatch "list" x
 connect x = throwError $ NumArgs 2 x
 
+recvDoc :: String
+recvDoc = "receive data from a socket <par>sock</par>. The maximum length can\n\
+be specified through <par>n</par>. It is possible for the length of the returned\n\
+data to be less than <par>n</par> if the data the socket currently holds is less.\n\
+\n\
+  params:\n\
+    - sock: the socket to receive data from\n\
+    - n: the maximum length of the data to be read\n\
+  complexity: O(n)\n\
+  returns: a byte vector"
+
 recv :: [LispVal] -> IOThrowsError LispVal
 recv [sock@(Opaque _),
       SimpleVal (Number (NumS n))] = do
@@ -120,6 +160,15 @@ recv [x, SimpleVal (Number (NumS _))] = throwError $ TypeMismatch "list" x
 recv [_, x] = throwError $ TypeMismatch "small-int" x
 recv x = throwError $ NumArgs 2 x
 
+sendDoc :: String
+sendDoc = "send <par>data</par> through a socket <par>sock</par>.\n\
+\n\
+  params:\n\
+    - sock: the socket through which data should be sent\n\
+    - data: the data to send (should be a byte vector)\n\
+  complexity: O(n)\n\
+  returns: the length of the data was actually sent"
+
 send :: [LispVal] -> IOThrowsError LispVal
 send [sock@(Opaque _),
       ByteVector b] = do
@@ -131,6 +180,16 @@ send [sock@(Opaque _),
 send [x, ByteVector _] = throwError $ TypeMismatch "list" x
 send [_, x] = throwError $ TypeMismatch "byte-vector" x
 send x = throwError $ NumArgs 2 x
+
+bindSocketDoc :: String
+bindSocketDoc = "bind a socket <par>sock</par> to an address <par>addr</par>\n\
+as defined by <fun>net:get-addr-info</par>.\n\
+\n\
+  params:\n\
+    - sock: the socket to bind\n\
+    - addr: the address to bind <par>sock</par> to\n\
+  complexity: O(1)\n\
+  returns: nil"
 
 bindSocket :: [LispVal] ->IOThrowsError LispVal
 bindSocket [sock@(Opaque _),
@@ -148,6 +207,15 @@ bindSocket [x, List _] = throwError $ TypeMismatch "opaque<socket>" x
 bindSocket [_, x] = throwError $ TypeMismatch "opaque<addrinfo>" x
 bindSocket x = throwError $ NumArgs 2 x
 
+listenDoc :: String
+listenDoc = "put a socket <par>sock</par> into listening mode, i.e. ready for\n\
+ingoing connections.\n\
+\n\
+  params:\n\
+    - sock: the socket to put into listening mode\n\
+  complexity: O(1)\n\
+  returns: nil"
+
 listen :: [LispVal] -> IOThrowsError LispVal
 listen [sock@(Opaque _),
         SimpleVal (Number (NumS n))] = do
@@ -163,6 +231,15 @@ listen [x, SimpleVal (Number (NumS _))] = throwError $ TypeMismatch "opaque<addr
 listen [_, x] = throwError $ TypeMismatch "small-int" x
 listen x = throwError $ NumArgs 2 x
 
+acceptDoc :: String
+acceptDoc = "accept an incoming connection through socket <par>sock</par>.\n\
+It will block until a connection comes in.\n\
+\n\
+  params:\n\
+    - sock: the socket through which connections should be accepted\n\
+  complexity: O(1) (blocks until a connection comes in)\n\
+  returns: a list of the form <zepto>[connection, address]</zepto>"
+
 accept :: [LispVal] -> IOThrowsError LispVal
 accept [sock@(Opaque _)] = do
     case ((fromOpaque sock) :: Maybe NS.Socket) of
@@ -172,6 +249,15 @@ accept [sock@(Opaque _)] = do
       Nothing -> throwError $ TypeMismatch "opaque<socket>" sock
 accept [x] = throwError $ TypeMismatch "opaque<socket>" x
 accept x = throwError $ NumArgs 1 x
+
+closeDoc :: String
+closeDoc = "close a socket. Frees the file descriptor.\n\
+It should not be reused after that.\n\
+\n\
+  params:\n\
+    - sock: the socket to close\n\
+  complexity: O(1)\n\
+  returns: nil"
 
 close :: [LispVal] -> IOThrowsError LispVal
 close [sock@(Opaque _)] = do
