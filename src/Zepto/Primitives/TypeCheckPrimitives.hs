d7ea1e76b98b88d0adb3f2deb7f2e7b8ae957370
hellerve <veit.heller@student.htw-berlin.de>
2017-01-17 12:15:18 +0100

    added type checkers for comprehensions

--- src/Zepto/Primitives/TypeCheckPrimitives.hs
+++ src/Zepto/Primitives/TypeCheckPrimitives.hs
@@ -175,3 +175,16 @@ checkType x = return $ fromSimple $ String $ typeString x
 isHash :: LispVal -> ThrowsError LispVal
 isHash (HashMap _) = return $ fromSimple $ Bool True
 isHash _ = return $ fromSimple $ Bool False
+
+isListComp :: LispVal -> ThrowsError LispVal
+isListComp (ListComprehension _ _ _ _) = return $ fromSimple $ Bool True
+isListComp _ = return $ fromSimple $ Bool False
+
+isHashComp :: LispVal -> ThrowsError LispVal
+isHashComp (HashComprehension _ _ _ _) = return $ fromSimple $ Bool True
+isHashComp _ = return $ fromSimple $ Bool False
+
+isComp :: LispVal -> ThrowsError LispVal
+isComp (ListComprehension _ _ _ _) = return $ fromSimple $ Bool True
+isComp (HashComprehension _ _ _ _) = return $ fromSimple $ Bool True
+isComp _ = return $ fromSimple $ Bool False
